26e913d97921b355e35cc8d9faf6e0a1391fe4ea
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-16 23:24:18 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-# 仅当存在镜像时才删除，避免空参数报错
+# 修复docker空镜像报错 专属适配雨夜模板
 imgs=$(docker images -q)
 if [ -n "$imgs" ]; then
   docker rmi -f $imgs
 fi
-
-# 清理空间
-sudo apt autoremove -y
+# 空间清理
+sudo apt autoremove -y >/dev/null 2>&1
 sudo apt clean
