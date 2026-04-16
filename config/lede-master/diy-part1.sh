a886cb0d4d0b71a106ea6991543ed0d662dbbc9a
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-16 23:21:25 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#
-# https://github.com/xinlingduyu/build-openwrt
-#
 
-# 清除旧镜像（修复报错：无镜像时不执行）
-if [ -n "$(docker images -q)" ]; then
-    docker rmi $(docker images -q)
+# 仅当存在镜像时才删除，避免空参数报错
+imgs=$(docker images -q)
+if [ -n "$imgs" ]; then
+  docker rmi -f $imgs
 fi
 
-# 清理系统空间
-df -h
+# 清理空间
+sudo apt autoremove -y
+sudo apt clean
