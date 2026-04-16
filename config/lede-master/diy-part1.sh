69732c5bbd0a23d61586796da6cb3fa02c5121c0
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-16 23:30:46 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# 修复docker空镜像报错 专属适配雨夜模板
-imgs=$(docker images -q)
-if [ -n "$imgs" ]; then
-  docker rmi -f $imgs
-fi
-# 空间清理
-sudo apt autoremove -y >/dev/null 2>&1
-sudo apt clean
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+#========================================================================================================================
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# other
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
