728aa33c8046db7fc385dc7d515efe49bd734d07
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-16 23:24:52 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -1,37 +1,17 @@
 #!/bin/bash
+# 定制版本信息
+sed -i "s/OpenWrt /CR6606-Lean-$(TZ=UTC-8 date +%Y.%m.%d) /g" package/lean/default-settings/files/zzz-default-settings
 
-# 修改默认IP（可选，保持 192.168.1.1 即可）
-sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+# 1.多WAN自动登录 Zesuy 官方
+git clone https://github.com/Zesuy/luci-app-multi-login.git package/luci-app-multi-login
 
-# 版本号里显示编译时间
-sed -i "s/OpenWrt /OpenWrt Build $(TZ=UTC-8 date +%Y.%m.%d) /g" package/lean/default-settings/files/zzz-default-settings
+# 2.校园网防检测 rkp-ipid
+git clone https://github.com/pexcn/openwrt-rkp-ipid.git package/rkp-ipid
 
-# 添加 多WAN自动登录插件（Zesuy 核心）
-git clone https://github.com/Zesuy/luci-app-multi-login package/luci-app-multi-login
+# 3.主题 & 增强插件
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# 添加 校园网防检测（rkp-ipid）
-git clone https://github.com/pexcn/openwrt-rkp-ipid package/rkp-ipid
-
-# 添加 闭源WiFi驱动（CR6606 信号增强）
-git clone https://github.com/openwrt/mt76.git package/mt76
-
-# 添加 主题 Argon 配置
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
-# 添加 AdGuard Home
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-
-# 添加 SmartDNS + DoH
-git clone https://github.com/pymumu/smartdns.git package/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-
-# 添加 常用工具
-git clone https://github.com/tty228/luci-app-wrtbwmon package/luci-app-wrtbwmon
-
-# 更新 feeds 并安装所有插件（必须）
+# 更新依赖
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
-# 权限设置
-chmod -R 755 package/luci-app-multi-login
-chmod -R 755 package/rkp-ipid
