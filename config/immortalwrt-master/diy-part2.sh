8bc4bc8599e83ace0bb28ba97d0e8fd14fd56fa2
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-17 19:44:55 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
-#========================================================================================================================
+cd openwrt
 
-# ------------------------------- Main source started -------------------------------
-#
-# Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+# 启用无线驱动
+sed -i 's/# CONFIG_PACKAGE_kmod-mt7915e is not set/CONFIG_PACKAGE_kmod-mt7915e=y/' .config
+sed -i 's/# CONFIG_PACKAGE_kmod-mt7621 is not set/CONFIG_PACKAGE_kmod-mt7621=y/' .config
 
-# Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
+# 启用 Argon 主题
+sed -i 's/CONFIG_DEFAULT_THEME.*/CONFIG_DEFAULT_THEME="argon"/' .config
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
-#
-# ------------------------------- Main source ends -------------------------------
+# 关闭默认主题
+sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/CONFIG_PACKAGE_luci-theme-bootstrap=n/' .config
 
-# ------------------------------- Other started -------------------------------
-#
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
-# Apply patch
-# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
-#
-# ------------------------------- Other ends -------------------------------
-
+# 网络优化
+echo "net.netfilter.nf_conntrack_max=65535" >> package/base-files/files/etc/sysctl.conf
+echo "net.ipv4.tcp_syncookies=1" >> package/base-files/files/etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse=1" >> package/base-files/files/etc/sysctl.conf
+echo "net.ipv4.tcp_window_scaling=1" >> package/base-files/files/etc/sysctl.conf
