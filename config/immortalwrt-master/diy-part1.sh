288fcd76347bc456418f4e5e5d03051c7eb58c9d
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-17 19:52:36 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd openwrt
 
-# 多WAN自动登录
+# 本脚本运行目录已经是 openwrt，无需 cd
+
+# 多WAN自动登录（Zesuy）
 git clone https://github.com/Zesuy/luci-app-multi-login package/luci-app-multi-login
 
 # OpenClash
@@ -13,6 +14,6 @@ git clone https://github.com/monsunpass/ua3f package/ua3f
 # RKP-IPID 防检测
 git clone https://github.com/EOIwork/rkp-ipid package/rkp-ipid
 
-# Argon 主题
+# Argon 主题 + 配置
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
