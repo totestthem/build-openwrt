2a9f352b23265ecf4a2875ce2995bdd38b729747
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-18 00:33:09 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -9,10 +9,10 @@ git clone https://github.com/Zesuy/luci-app-multi-login package/luci-app-multi-l
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # UA3F
-git clone https://github.com/monsunpass/ua3f package/ua3f
+git clone https://github.com/SunBK201/ua3f package/ua3f
 
 # RKP-IPID 防检测
-git clone https://github.com/EOIwork/rkp-ipid package/rkp-ipid
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 
 # Argon 主题 + 配置
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
