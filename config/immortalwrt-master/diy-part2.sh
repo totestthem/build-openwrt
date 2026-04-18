15838ca205a7daf131329aa9ba5f0073145a3a16
totestthem <143327319+totestthem@users.noreply.github.com>
2026-04-18 11:33:26 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -16,6 +16,8 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> package/base-files/files/etc/sysct
 
 #添加多wan自动登录
 echo "CONFIG_PACKAGE_luci-app-multi-login=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+#echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 echo "CONFIG_PACKAGE_ua3f=y" >> .config
 echo "CONFIG_PACKAGE_rkp-ipid=y" >> .config
+
+echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> .config
